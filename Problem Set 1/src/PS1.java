08e7261ec817abc03d41ee17474844bcf27a1ac9
Jerick Gilla-Fowler <jgilla100@uafs.edu>
2026-03-09 10:58:58 -0500

    figured out importing files in arrays of similar sizes

--- Problem Set 1/src/PS1.java
+++ Problem Set 1/src/PS1.java
@@ -1,6 +1,6 @@
 /*
- * Name: 					Jerick Gilla-Fowler
- * Problem Set: 	PS1
+ * Name: 				Jerick Gilla-Fowler
+ * Problem Set: 		PS1
  * Due Date: 			March 11, 2026
  */
 public class PS1 {
@@ -16,7 +16,12 @@ public class PS1 {
 			{2, 1, 2},
 			{1, 1, 2} };	
 		
-		System.out.println(calculateGrid(a, b, 1, 1));
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[1].length; j++) {
+				System.out.print(calculateGrid(a, b, i, j) + " ");
+			}
+			System.out.println();
+		}
 	}
 	
 	public static int calculateGrid(int[][] a, int[][] b, int x, int y) {
@@ -25,15 +30,17 @@ public class PS1 {
 		int yCount = 0;
 		
 		// Moving coords up and to the left.
+
 		x--;
 		y--;
 		
 		for(int i = y; i < y + 3; i++) {
 			for(int j = x; j < x + 3; j++) {
-				if( x == -1 || y == -1 || x == a[0].length || y == a.length ) {
+				if( j == -1 || i == -1 || j == a[0].length || i == a.length ) {
 					sum += 0 * b[yCount][xCount];
+				}else {
+					sum += a[i][j] * b[yCount][xCount];
 				}
-				sum += a[i][j] * b[yCount][xCount];
 				xCount++;
 			}
 			yCount++;
