788a4d4c87fb47ae55350c2d2e5dd474ab110755
lodi429 <39424080+lodi429@users.noreply.github.com>
2026-03-10 20:10:32 -0500

    added conversion from file to array
    now focusing on main problem set after figuring out what i needed from
    the test file. split fileLength and fileToArray into separate arrays to
    prevent fileToArray from becoming bloated, now onto the last stretch of
    making sure everything works

--- Problem Set 1/src/PS1.java
+++ Problem Set 1/src/PS1.java
@@ -3,25 +3,25 @@
  * Problem Set: 		PS1
  * Due Date: 			March 11, 2026
  */
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 public class PS1 {
 	public static void main(String[] args) {
-		int[][] a = {
-			{3, 5, 2, 7},
-			{4, 1, 3, 8},
-			{6, 3, 8, 2},
-			{9, 6, 1, 5} };
-		
-		int[][] b = {
-			{1, 2, 1},
-			{2, 1, 2},
-			{1, 1, 2} };	
-		
-		for (int i = 0; i < a.length; i++) {
-			for (int j = 0; j < a[1].length; j++) {
-				System.out.print(calculateGrid(a, b, i, j) + " ");
+		int[][] a = fileToArray(args[0]);
+		int[][] b = fileToArray(args[1]);
+		for(int i = 0; i < a.length; i++){
+			for(int j = 0; j < a[0].length; j++){
+				System.out.print(a[i][j] + " ");
 			}
 			System.out.println();
-		}
+		}		
+		for(int i = 0; i < b.length; i++){
+			for(int j = 0; j < b[0].length; j++){
+				System.out.print(b[i][j] + " ");
+			}
+			System.out.println();
+		}		
 	}
 	
 	public static int calculateGrid(int[][] a, int[][] b, int x, int y) {
@@ -48,4 +48,49 @@ public class PS1 {
 		}
 		return sum;
 	}
+	public static int[][] fileToArray(String file){
+		/*
+		 * 
+		 */
+	  try {
+		  int length = fileLength(file);
+		  Scanner newFile = new Scanner(new FileReader(file));
+		  int[][] fileArray = new int[length][0];
+		  while(newFile.hasNext()){
+		  	for(int i = 0; i < fileArray.length; i++){
+		  		String[] line = newFile.nextLine().split(" ");
+		  		int[] nums = new int[line.length];
+		  		for(int j = 0; j < line.length; j++){
+						nums[j] = Integer.parseInt(line[j]);
+		  		}
+		  		fileArray[i] = nums;
+		  	}
+		  }
+		  newFile.close();
+		  return fileArray;
+	  }catch(FileNotFoundException e) {
+		  System.out.println("File Not Found");
+		  return null;
+	  }
+  }
+  public static int fileLength(String file){
+  	/*
+  	 * Separating fileLength into its own method
+  	 * to keep fileToArray from becoming too bloated
+  	 * Reads how many lines a file has.
+  	 */
+  	try{
+  		Scanner filee = new Scanner(new FileReader(file));
+		  int count = 0;
+		  while(filee.hasNext()) {
+		   	count++;
+		   	filee.nextLine();
+		  }
+		  filee.close();
+		  return count;
+  	}catch(FileNotFoundException e) {
+  		System.out.println("File Not Found");
+  		return -1;
+  	}
+  }
 }
